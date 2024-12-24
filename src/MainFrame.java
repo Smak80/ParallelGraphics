eddba49192ae2004d07b7677647fe57c906bdc89
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2024-12-24 14:55:17 +0300

    Синхронизация потоков

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
-public class MainFrame {
+public class MainFrame extends JFrame{
+
+    private final JPanel mainPanel = new JPanel(){
+        @Override
+        public void paint(Graphics g){
+            var count = 10;
+            var width = mainPanel.getWidth() / count;
+            var gr = mainPanel.getGraphics();
+            for (int i = 0; i < count; i++){
+                StripePainter sPainter = new StripePainter(
+                        gr,
+                        i * width,
+                        width,
+                        mainPanel.getHeight()
+                );
+                new Thread(sPainter).start();
+            }
+        }
+
+    };
+
+    public MainFrame(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(600, 500));
+        add(mainPanel);
+        mainPanel.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+                mainPanel.repaint();
+            }
+        });
+    }
 }
--- src/StripePainter.java
+++ src/StripePainter.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
-public class StripePainter {
+public class StripePainter implements Runnable{
+
+    private final Graphics g;
+    private int start;
+    private int width;
+    private int height;
+    private Color color;
+    private BufferedImage img;
+
+    public StripePainter(Graphics g, int start, int width, int height){
+        this.g = g;
+        this.start = start;
+        this.width = width;
+        this.height = height;
+        color = new Color(
+                (float)Math.random(),
+                (float)Math.random(),
+                (float)Math.random()
+        );
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    }
+
+    @Override
+    public void run() {
+        var g = img.createGraphics();
+        for (int i = 0; i < width; i++){
+            for (int j = 0; j < height; j++){
+                g.setColor(color);
+                g.fillRect(i, j, 1, 1);
+            }
+        }
+        synchronized (this.g){
+            this.g.drawImage(img, start, 0, null);
+        }
+    }
 }
